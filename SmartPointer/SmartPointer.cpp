06db066d6fe63326b67bcbc85e69aac2f45e97b8
Kaan Dönmez <kndonmezz@gmail.com>
2021-12-15 21:21:55 +0300

    Smart pointer updated.

--- SmartPointer/SmartPointer.cpp
+++ SmartPointer/SmartPointer.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 #include <memory>
+#include <string>
+
+class Istanbul
+{
+public:
+    Istanbul()
+    {
+        std::cout << "Istanbul Created" << std::endl;
+    }
+    ~Istanbul()
+    {
+        std::cout << "Istanbul Destroyed" << std::endl;
+    }
+};
+
+class Ankara
+{
+public:
+    Ankara()
+    {
+        std::cout << "Ankara Created" << std::endl;
+    }
+
+    ~Ankara()
+    {
+        std::cout << "Ankara Destroyed" << std::endl;
+    }
+};
 
 int main()
 {
+    {
+        std::unique_ptr<Istanbul> entity(new Istanbul());
+    }
+    {
+        Ankara *p = new Ankara();
+        delete p;
+    }
+
+    std::cin.get();
 }
